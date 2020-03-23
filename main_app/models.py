e350f0feed47147cac68b60350d8cf3a9b6f7cd9
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-03-23 13:34:15 -0600

    slogan

--- main_app/models.py
+++ main_app/models.py
@@ -4,11 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 class App(models.Model):
     name = models.CharField(max_length=25)
-    description = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    slogan = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
 class Comment(models.Model):
     words = models.TextField(blank=True)
-    
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
