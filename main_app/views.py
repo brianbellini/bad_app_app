b08dbc12063e43f1feb70ab2cb198775a5a7334c
brianbellini <brianbellini@me.com>
2020-03-23 16:22:00 -0600

    Reworked signup(commented out)

--- main_app/views.py
+++ main_app/views.py
@@ -5,10 +5,12 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import App, Comment
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -59,16 +61,34 @@ class CommentDelete(LoginRequiredMixin, DeleteView):
   success_url = '/apps/'
 
 #--------------------ACCOUNTS-----------------------------
-def signup(request):
-    error_message = ''
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('index')
-        else:
-            error_message = 'Invalid sign up - try again'
-    form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
+# def signup(request):
+#     error_message = ''
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('index')
+#         else:
+#             error_message = 'Invalid sign up - try again'
+#     form = UserCreationForm()
+#     context = {'form': form, 'error_message': error_message}
+#     return render(request, 'registration/signup.html', context)
+
+# class RegisterForm(UserCreationForm):
+#     email = forms.EmailField(label = 'Email')
+#     first_name = forms.CharField(label = 'First name')
+#     last_name = forms.CharField(label= 'Last name')
+
+#     class Meta:
+#         model = User
+#         fields = ('username', 'first_name', 'last_name', 'email',)
+
+# def save(self, commit=True):
+#     user = super(RegisterForm, self).save(commit-False)
+#     user.first_name = first_name
+#     user.last_name = last_name
+#     user.email = self.cleaned_data["email"]
+#     if commit:
+#         user.save()
+#     return user
