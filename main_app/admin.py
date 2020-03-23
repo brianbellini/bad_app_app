5b36921bdcc88c0b507e6b3bf746b7839e71526b
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-03-23 16:24:47 -0600

    tag, comment, vote models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import App, Comment
+from .models import App, Comment, Tag, Vote
 
 # Register your models here.
 admin.site.register(App)
 admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Vote)
+
--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
 class App(models.Model):
     name = models.CharField(max_length=25)
     description = models.TextField(blank=True)
-    slogan = models.TextField(blank=True)
+    slogan = models.CharField(max_length=50)
+    group = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
@@ -13,3 +22,8 @@ class App(models.Model):
 class Comment(models.Model):
     words = models.TextField(blank=True)
     app = models.ForeignKey(App, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+class Vote(models.Model):
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
