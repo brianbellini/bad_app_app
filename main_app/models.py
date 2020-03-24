ac6e30360d29160c51af513a51feee0e606b462f
Cody Hart <codyhart@Codys-MacBook-Pro-5.local>
2020-03-24 17:12:50 -0600

    Adds update functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,29 +1,70 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 VOTE_VALUES = {
     (1, 'GOOD'),
     ((-1), 'BAD')
 }
 
-# Create your models here.
-class Tag(models.Model):
-    name = models.CharField(max_length=25)
+TAGS = (
+    ('As', 'Astronauts'),
+    ('Bo', 'Books'),
+    ('Bu', 'Business'),
+    ('Cr', 'Crime'),
+    ('Dr', 'Drugs'),
+    ('Ed', 'Education'),
+    ('En', 'Entertainment'),
+    ('Fi', 'Finance'),
+    ('Fo', 'Food & Drink'),
+    ('He', 'Health & Fitness'),
+    ('Je', 'Jesus'),
+    ('Ki', 'Kids'),
+    ('Li', 'Lifestyle'),
+    ('Ma', 'Magazines & Newspapers'),
+    ('Me', 'Medical'),
+    ('Mu', 'Music'),
+    ('Na', 'Navigation'),
+    ('Ne', 'News'),
+    ('Ns', 'NSFW'),
+    ('Ph', 'Photo & Video'),
+    ('Pr', 'Productivity'),
+    ('Re', 'Reference'),
+    ('Sh', 'Shopping'),
+    ('So', 'Social Networking'),
+    ('Sp', 'Sports'),
+    ('Tr', 'Travel'),
+    ('Ut', 'Utilities'),
+    ('We', 'Weather'),
+    ('Ot', 'Other'),
+)
 
-    def __str__(self):
-        return self.name
+# Create your models here.
 
 class App(models.Model):
     name = models.CharField(max_length=25)
     description = models.TextField(blank=True)
     slogan = models.CharField(max_length=50)
     group = models.CharField(max_length=50)
+    tag = models.CharField(
+        max_length=2,
+        choices=TAGS,
+        default=TAGS[-1][0]
+        )
+    net_votes = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tag = models.ManyToManyField(Tag)
-    net_votes = models.IntegerField()
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'app_id': self.id})
+    
+    # def get_net_votes(self):
+    #     all_votes = Vote.objects.filter(app=self.id)
+    #     net = 0
+    #     for vote in all_votes:
+    #         net += vote.value
 
 class Comment(models.Model):
     words = models.TextField(blank=True)
